e19304e72ca7543b6d004463caef7db274ef4356
web3nomad.eth <me@web3nomad.eth>
2024-10-02 21:36:28 +0800

    modity prompt to support text-only or image with text input

--- src/ort.rs
+++ src/ort.rs
@@ -2,15 +2,21 @@ use std::borrow::Cow;
 
 use crate::phi3v::{image_process::Phi3VImageProcessor, text_process::Phi3VTextProcessor};
 use anyhow::Result;
-use ndarray::{Array, Array3, ArrayView, Axis};
-use ort::{GraphOptimizationLevel, Session, SessionInputValue, Tensor};
+use ndarray::{Array, ArrayView, Axis, Ix3};
+use ort::{
+    // GraphOptimizationLevel,
+    Session,
+    SessionInputValue,
+    Tensor,
+};
 
 pub async fn run() -> Result<()> {
-    let visual_features = {
+    #[allow(unused_assignments)]
+    let mut img = Some(image::open("./models/frames/4000.jpg").unwrap()); //./models/20240923-173209.jpeg
+    img = None;
+    let visual_features = if let Some(img) = &img {
         let image_processor = Phi3VImageProcessor::new();
-        // let img = image::open("./models/20240923-173209.jpeg").unwrap();
-        let img = image::open("./models/frames/4000.jpg").unwrap();
-        let result = image_processor.preprocess(&img)?;
+        let result = image_processor.preprocess(img)?;
         println!(
             "image process result, num_img_tokens: {num_img_tokens:?}, pixel_values: {pixel_values:?}, image_sizes: {image_sizes:?}",
             num_img_tokens = result.num_img_tokens,
@@ -28,27 +34,27 @@ pub async fn run() -> Result<()> {
         let outputs = model.run(model_inputs)?;
         let predictions_view: ArrayView<f32, _> =
             outputs["visual_features"].try_extract_tensor::<f32>()?;
-        let shape = predictions_view.shape();
-        if shape.len() != 3 {
-            return Err(anyhow::anyhow!("Expected 3D array, got {}D", shape.len()));
-        }
-        let predictions: Array3<f32> = Array::from_shape_vec(
-            (shape[0], shape[1], shape[2]),
-            predictions_view.iter().cloned().collect(),
-        )
-        .map_err(|_| anyhow::anyhow!("Failed to create Array3 from predictions"))?;
+        let predictions = predictions_view.into_dimensionality::<Ix3>()?.to_owned();
         predictions
+    } else {
+        Array::zeros((1, 0, 0))
     };
     // println!("visual_features {:?}", visual_features);
 
     let text_processor = Phi3VTextProcessor::new("models/phi-3-vision/tokenizer.json")?;
     let (text_inputs_embeds, text_attention_mask) = {
         // 参考了 https://github.com/microsoft/onnxruntime-genai/blob/main/examples/python/phi3v.py
-        let text = format!(
-            "<|image_1|>\n{prompt}",
-            prompt = "Describe the image in detail."
-        );
+        let text = if let Some(_img) = &img {
+            format!(
+                "<|image_1|>\n{prompt}",
+                prompt = "Describe the image in detail."
+            )
+        } else {
+            format!("{prompt}", prompt = "Who are you?")
+        };
         let (input_ids, attention_mask) = text_processor.preprocess(&text)?;
+        println!("input_ids: {:?}", input_ids);
+        println!("attention_mask: {:?}", attention_mask);
         let model_inputs = ort::inputs![
             "input_ids" => input_ids,
         ]?;
@@ -59,15 +65,26 @@ pub async fn run() -> Result<()> {
         let outputs = model.run(model_inputs)?;
         let predictions_view: ArrayView<f32, _> =
             outputs["inputs_embeds"].try_extract_tensor::<f32>()?;
-        let shape = predictions_view.shape();
-        if shape.len() != 3 {
-            return Err(anyhow::anyhow!("Expected 3D array, got {}D", shape.len()));
-        }
-        let predictions: Array3<f32> = Array::from_shape_vec(
-            (shape[0], shape[1], shape[2]),
-            predictions_view.iter().cloned().collect(),
-        )
-        .map_err(|_| anyhow::anyhow!("Failed to create Array3 from predictions"))?;
+        // println!("predictions_view: {:?}", predictions_view);
+        // {
+        //     let shape = predictions_view.shape();
+        //     for i in 0..shape[0] {
+        //         for j in 0..shape[1] {
+        //             let row = predictions_view.slice(ndarray::s![i, j, ..]);
+        //             println!("predictions[{}][{}]: {:?}", i, j, row);
+        //         }
+        //     }
+        // }
+        let predictions = predictions_view.into_dimensionality::<Ix3>()?.to_owned();
+        // let shape = predictions_view.shape();
+        // if shape.len() != 3 {
+        //     return Err(anyhow::anyhow!("Expected 3D array, got {}D", shape.len()));
+        // }
+        // let predictions: Array3<f32> = Array::from_shape_vec(
+        //     (shape[0], shape[1], shape[2]),
+        //     predictions_view.iter().cloned().collect(),
+        // )
+        // .map_err(|_| anyhow::anyhow!("Failed to create Array3 from predictions"))?;
         (predictions, attention_mask)
     };
 
@@ -75,29 +92,30 @@ pub async fn run() -> Result<()> {
     // println!("attention_mask {:?}", attention_mask);
 
     let logits = {
-        // 确保两个数组在批次维度和嵌入维度上匹配
-        assert_eq!(text_inputs_embeds.shape()[0], visual_features.shape()[0]);
-        assert_eq!(text_inputs_embeds.shape()[2], visual_features.shape()[2]);
-
-        // 在时间维度（通常是第二个维度，索引为 1）上拼接
-        let combined_features = ndarray::concatenate(
-            Axis(1),
-            &[text_inputs_embeds.view(), visual_features.view()],
-        )?;
-
-        // 更新 attention_mask
-        let visual_attention =
-            Array::ones((text_attention_mask.shape()[0], visual_features.shape()[1]));
-        let combined_attention_mask = ndarray::concatenate(
-            Axis(1),
-            &[text_attention_mask.view(), visual_attention.view()],
-        )?;
+        // // 确保两个数组在批次维度和嵌入维度上匹配
+        let (combined_features, combined_attention_mask) = if let Some(_img) = &img {
+            assert_eq!(text_inputs_embeds.shape()[0], visual_features.shape()[0]);
+            assert_eq!(text_inputs_embeds.shape()[2], visual_features.shape()[2]);
+            // 在时间维度（通常是第二个维度，索引为 1）上拼接
+            let combined_features = ndarray::concatenate(
+                Axis(1),
+                &[text_inputs_embeds.view(), visual_features.view()],
+            )?;
+            // 更新 attention_mask
+            let visual_attention =
+                Array::ones((text_attention_mask.shape()[0], visual_features.shape()[1]));
+            let combined_attention_mask = ndarray::concatenate(
+                Axis(1),
+                &[text_attention_mask.view(), visual_attention.view()],
+            )?;
+            (combined_features, combined_attention_mask)
+        } else {
+            (text_inputs_embeds, text_attention_mask)
+        };
 
         let mut model_inputs = ort::inputs![
             "inputs_embeds" => combined_features,
             "attention_mask" => combined_attention_mask,
-            // "inputs_embeds" => text_inputs_embeds,
-            // "attention_mask" => text_attention_mask,
         ]?;
 
         let num_layers = 32;
@@ -109,8 +127,11 @@ pub async fn run() -> Result<()> {
         // Create empty past key and value tensors
         let past_key_shape = vec![batch_size, num_heads, sequence_length, head_size];
         let past_value_shape = past_key_shape.clone();
+        println!("past_key_shape: {:?}", past_key_shape);
+        println!("past_value_shape: {:?}", past_value_shape);
 
         let empty_past_state = vec![0.0f32; past_key_shape.iter().product()];
+        println!("empty_past_state: {:?}", empty_past_state);
         // Add past key-value pairs for each layer
         for i in 0..num_layers {
             let past_key_tensor = Tensor::from_array(Array::from_shape_vec(
@@ -121,7 +142,12 @@ pub async fn run() -> Result<()> {
                 past_value_shape.clone(),
                 empty_past_state.clone(),
             )?)?;
-
+            // {
+            //     let v = &past_key_tensor.try_extract_tensor::<f32>();
+            //     println!("past_key_tensor[{}]: {:?}", i, v);
+            //     let v = &past_value_tensor.try_extract_tensor::<f32>();
+            //     println!("past_value_shape[{}]: {:?}", i, v);
+            // }
             let key: Cow<'_, str> = format!("past_key_values.{}.key", i).into();
             let val: SessionInputValue<'_> = past_key_tensor.into();
             model_inputs.push((key, val));
@@ -138,15 +164,7 @@ pub async fn run() -> Result<()> {
         let outputs = model.run(model_inputs)?;
         // println!("outputs {:?}", outputs);
         let predictions_view: ArrayView<f32, _> = outputs["logits"].try_extract_tensor::<f32>()?;
-        let shape = predictions_view.shape();
-        if shape.len() != 3 {
-            return Err(anyhow::anyhow!("Expected 3D array, got {}D", shape.len()));
-        }
-        let predictions: Array3<f32> = Array::from_shape_vec(
-            (shape[0], shape[1], shape[2]),
-            predictions_view.iter().cloned().collect(),
-        )
-        .map_err(|_| anyhow::anyhow!("Failed to create Array3 from predictions"))?;
+        let predictions = predictions_view.into_dimensionality::<Ix3>()?.to_owned();
         predictions
     };
     // println!("logits {:?}", logits);
