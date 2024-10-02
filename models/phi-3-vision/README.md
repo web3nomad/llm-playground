988fa69ed7699d2aebe9b3574e0461980de84426
web3nomad.eth <me@web3nomad.eth>
2024-10-02 21:00:47 +0800

    commit phi3v.py

---
+++ models/phi-3-vision/README.md
@@ -0,0 +1,7 @@
+
+```bash
+docker run \
+    --platform linux/amd64 \
+    -v ~/workspace/rust/llm-playground/models/phi-3-vision/:/workspace/phi-3-vision/ \
+    -i -t python3-shell-amd64 /bin/bash
+```
---
+++ models/phi-3-vision/phi3v.py
@@ -0,0 +1,81 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License
+
+import argparse
+import os
+import readline
+import glob
+
+import onnxruntime_genai as og
+
+def _complete(text, state):
+    return (glob.glob(text + "*") + [None])[state]
+
+
+def run(args: argparse.Namespace):
+    print("Loading model...")
+    model = og.Model(args.model_path)
+    processor = model.create_multimodal_processor()
+    tokenizer_stream = processor.create_stream()
+
+    while True:
+        readline.set_completer_delims(" \t\n;")
+        readline.parse_and_bind("tab: complete")
+        readline.set_completer(_complete)
+        image_paths = [
+            image_path.strip()
+            for image_path in input(
+                "Image Path (comma separated; leave empty if no image): "
+            ).split(",")
+        ]
+        image_paths = [image_path for image_path in image_paths if len(image_path)]
+        print(image_paths)
+
+        images = None
+        prompt = "<|user|>\n"
+        if len(image_paths) == 0:
+            print("No image provided")
+        else:
+            print("Loading images...")
+            for i, image_path in enumerate(image_paths):
+                if not os.path.exists(image_path):
+                    raise FileNotFoundError(f"Image file not found: {image_path}")
+                prompt += f"<|image_{i+1}|>\n"
+
+            images = og.Images.open(*image_paths)
+
+        text = input("Prompt: ")
+        prompt += f"{text}<|end|>\n<|assistant|>\n"
+        print("Processing images and prompt...")
+        inputs = processor(prompt, images=images)
+        print("Prompt: ", prompt)
+        print("Inputs: ", inputs)
+
+        print("Generating response...")
+        params = og.GeneratorParams(model)
+        params.set_inputs(inputs)
+        params.set_search_options(max_length=7680)
+
+        generator = og.Generator(model, params)
+
+        while not generator.is_done():
+            generator.compute_logits()
+            generator.generate_next_token()
+
+            new_token = generator.get_next_tokens()[0]
+            print(tokenizer_stream.decode(new_token), end="", flush=True)
+
+        for _ in range(3):
+            print()
+
+        # Delete the generator to free the captured graph before creating another one
+        del generator
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-m", "--model_path", type=str, required=True, help="Path to the model"
+    )
+    args = parser.parse_args()
+    run(args)
