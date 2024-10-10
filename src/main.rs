6716c580ce63ed7fba222b16f5861fa7b7641dc5
web3nomad.eth <me@web3nomad.eth>
2024-10-10 16:27:26 +0800

    add gguf

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,17 @@ use anyhow::Result;
 #[allow(dead_code)]
 mod candle;
 #[allow(dead_code)]
+mod gguf;
+#[allow(dead_code)]
 mod mistral;
 #[allow(dead_code)]
 mod phi3v;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    mistral::run().await?;
+    // mistral::run().await?;
     // candle::run().await?;
     // phi3v::run().await?;
+    gguf::run().await?;
     Ok(())
 }
