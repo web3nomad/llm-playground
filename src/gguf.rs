41317da6fbe9566517c1b087204a713467c34a35
web3nomad.eth <me@web3nomad.eth>
2024-10-12 13:06:48 +0800

    try quantized llama

--- src/gguf.rs
+++ src/gguf.rs
@@ -1,66 +1,147 @@
-use anyhow::Result;
-use either::Either;
-use indexmap::IndexMap;
-use mistralrs::{
-    Constraint, DefaultSchedulerMethod, Device, DeviceMapMetadata, GGUFLoaderBuilder,
-    GGUFSpecificConfig, GgufModelBuilder, MistralRs, MistralRsBuilder, ModelDType, NormalRequest,
-    Request, RequestMessage, ResponseOk, SamplingParams, SchedulerConfig, TokenSource,
-    VisionLoaderBuilder, VisionLoaderType, VisionSpecificConfig,
-};
-use std::sync::Arc;
-use tokio::sync::mpsc::channel;
+use std::io::Write;
+use tokenizers::Tokenizer;
 
-pub async fn run() -> Result<()> {
-    let vision_loader = VisionLoaderBuilder::new(
-        VisionSpecificConfig {
-            use_flash_attn: false,
-            prompt_batchsize: None,
-            topology: None,
-            write_uqff: None,
-            from_uqff: None,
-        },
-        None,
-        None,
-        Some("xtuner/llava-phi-3-mini-hf".to_string()),
-    )
-    .build(VisionLoaderType::LLaVA);
+use candle_core::quantized::gguf_file;
+use candle_core::{Device, Tensor};
+use candle_examples::token_output_stream::TokenOutputStream;
+use candle_transformers::generation::{LogitsProcessor, Sampling};
+use candle_transformers::models::quantized_llama as model;
+use model::ModelWeights;
 
-    let vision_pipeline = vision_loader.load_model_from_hf(
-        None,
-        TokenSource::CacheToken,
-        &ModelDType::Auto,
-        &Device::cuda_if_available(0)?,
-        false,
-        DeviceMapMetadata::dummy(),
-        None,
-        None, // No PagedAttention.
-    )?;
+#[derive(Debug)]
+enum Prompt {
+    Interactive,
+    Chat,
+    One(String),
+}
 
-    let gguf_loader = GGUFLoaderBuilder::new(
-        Some("models/phi3.json".to_string()),
-        None,
-        "./models".to_string(),
-        vec![
-            "llava-phi-3-mini-int4.gguf".to_string(),
-            // "llava-phi-3-mini-mmproj-f16.gguf".to_string(),
-        ],
-        GGUFSpecificConfig {
-            prompt_batchsize: None,
-            topology: None,
-        },
-    )
-    .build();
+fn format_size(size_in_bytes: usize) -> String {
+    if size_in_bytes < 1_000 {
+        format!("{}B", size_in_bytes)
+    } else if size_in_bytes < 1_000_000 {
+        format!("{:.2}KB", size_in_bytes as f64 / 1e3)
+    } else if size_in_bytes < 1_000_000_000 {
+        format!("{:.2}MB", size_in_bytes as f64 / 1e6)
+    } else {
+        format!("{:.2}GB", size_in_bytes as f64 / 1e9)
+    }
+}
 
-    let gguf_pipeline = gguf_loader.load_model_from_hf(
-        None,
-        TokenSource::CacheToken,
-        &ModelDType::Auto,
-        &Device::cuda_if_available(0)?,
-        false,
-        DeviceMapMetadata::dummy(),
-        None,
-        None, // No PagedAttention.
-    )?;
+pub async fn run() -> anyhow::Result<()> {
+    let model_path = std::path::PathBuf::from("models/llava-phi-3/llava-phi-3-mini-int4.gguf");
+    let mut file = std::fs::File::open(&model_path)?;
+    let start = std::time::Instant::now();
+    let device = Device::new_metal(0)?;
+
+    let mut model = {
+        let model = gguf_file::Content::read(&mut file).map_err(|e| e.with_path(model_path))?;
+        let mut total_size_in_bytes = 0;
+        for (_, tensor) in model.tensor_infos.iter() {
+            let elem_count = tensor.shape.elem_count();
+            total_size_in_bytes +=
+                elem_count * tensor.ggml_dtype.type_size() / tensor.ggml_dtype.block_size();
+        }
+        println!(
+            "loaded {:?} tensors ({}) in {:.2}s",
+            model.tensor_infos.len(),
+            &format_size(total_size_in_bytes),
+            start.elapsed().as_secs_f32(),
+        );
+        ModelWeights::from_gguf(model, &mut file, &device)?
+    };
+    println!("model built");
+
+    let tokenizer =
+        Tokenizer::from_file("models/llava-phi-3/tokenizer.json").map_err(anyhow::Error::msg)?;
+    let mut tos = TokenOutputStream::new(tokenizer);
+    let prompt = "hello";
+
+    let prompt_str = format!("<|system|>\n</end>\n<|user|>\n{prompt}</end>\n<|assistant|>",);
+    println!("{}", &prompt_str);
+    let tokens = tos
+        .tokenizer()
+        .encode(prompt_str, true)
+        .map_err(anyhow::Error::msg)?;
+
+    let prompt_tokens = tokens.get_ids().to_vec();
+    let to_sample = 1000; // TODO: make this a parameter
+    let prompt_tokens = if prompt_tokens.len() + to_sample > model::MAX_SEQ_LEN - 10 {
+        let to_remove = prompt_tokens.len() + to_sample + 10 - model::MAX_SEQ_LEN;
+        prompt_tokens[prompt_tokens.len().saturating_sub(to_remove)..].to_vec()
+    } else {
+        prompt_tokens
+    };
+    let mut all_tokens = vec![];
+    let mut logits_processor = {
+        let temperature = 0.0; // TODO: make this a parameter
+        let sampling = if temperature <= 0. {
+            Sampling::ArgMax
+        } else {
+            Sampling::All { temperature }
+        };
+        let seed = 299792458; // TODO: make this a parameter
+        LogitsProcessor::from_sampling(seed, sampling)
+    };
+
+    let start_prompt_processing = std::time::Instant::now();
+    let mut next_token = {
+        let input = Tensor::new(prompt_tokens.as_slice(), &device)?.unsqueeze(0)?;
+        let logits = model.forward(&input, 0)?;
+        let logits = logits.squeeze(0)?;
+        logits_processor.sample(&logits)?
+    };
+    let prompt_dt = start_prompt_processing.elapsed();
+    all_tokens.push(next_token);
+    if let Some(t) = tos.next_token(next_token)? {
+        print!("{t}");
+        std::io::stdout().flush()?;
+    }
+
+    let eos_token = "<|end|>";
+    let eos_token = *tos.tokenizer().get_vocab(true).get(eos_token).unwrap();
+    let start_post_prompt = std::time::Instant::now();
+    let mut sampled = 0;
+    for index in 0..to_sample {
+        let input = Tensor::new(&[next_token], &device)?.unsqueeze(0)?;
+        let logits = model.forward(&input, prompt_tokens.len() + index)?;
+        let logits = logits.squeeze(0)?;
+        let repeat_penalty = 1.1; // TODO: make this a parameter
+        let repeat_last_n = 64; // TODO: make this a parameter
+        let logits = if repeat_penalty == 1. {
+            logits
+        } else {
+            let start_at = all_tokens.len().saturating_sub(repeat_last_n);
+            candle_transformers::utils::apply_repeat_penalty(
+                &logits,
+                repeat_penalty,
+                &all_tokens[start_at..],
+            )?
+        };
+        next_token = logits_processor.sample(&logits)?;
+        all_tokens.push(next_token);
+        if let Some(t) = tos.next_token(next_token)? {
+            print!("{t}");
+            std::io::stdout().flush()?;
+        }
+        sampled += 1;
+        if next_token == eos_token {
+            break;
+        };
+    }
+    if let Some(rest) = tos.decode_rest().map_err(candle_core::Error::msg)? {
+        print!("{rest}");
+    }
+    std::io::stdout().flush()?;
+    let dt = start_post_prompt.elapsed();
+    println!(
+        "\n\n{:4} prompt tokens processed: {:.2} token/s",
+        prompt_tokens.len(),
+        prompt_tokens.len() as f64 / prompt_dt.as_secs_f64(),
+    );
+    println!(
+        "{sampled:4} tokens generated: {:.2} token/s",
+        sampled as f64 / dt.as_secs_f64(),
+    );
 
     Ok(())
 }
