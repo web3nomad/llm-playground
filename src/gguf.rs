b658f290e51aa21214b4c1590912a5c140e69418
web3nomad.eth <me@web3nomad.eth>
2024-10-15 21:42:03 +0800

    move vision tokens processing to clip.rs

--- src/gguf.rs
+++ src/gguf.rs
@@ -1,14 +1,11 @@
 use super::qllava::{
-    format_prompt, load_qllama_model, HFPreProcessorConfig, ImageProcessor, QLlama,
+    format_prompt, load_clip, load_image_to_tensor, load_qllama_model,
+    prepare_inputs_labels_for_multimodal, tokenizer_image_token, QLlama,
 };
-use candle_core::{DType, Device, IndexOp, Tensor};
+use candle_core::{Device, IndexOp, Tensor};
 use candle_examples::token_output_stream::TokenOutputStream;
-use candle_transformers::{
-    generation::LogitsProcessor,
-    models::{clip::vision_model::ClipVisionConfig, quantized_llama},
-};
+use candle_transformers::{generation::LogitsProcessor, models::quantized_llama};
 use std::io::Write;
-use tokenizers::Tokenizer;
 
 pub fn generate_legacy(
     device: &Device,
@@ -126,210 +123,6 @@ pub fn generate(
     Ok(())
 }
 
-fn load_image_to_tensor(
-    device: &Device,
-    image_file_path: &str,
-    preprocessor_config_file_path: &str,
-) -> anyhow::Result<((u32, u32), Tensor)> {
-    let preprocessor_config: HFPreProcessorConfig =
-        serde_json::from_slice(&std::fs::read(preprocessor_config_file_path)?)?;
-    let image_processor = ImageProcessor::from_hf_preprocessor_config(&preprocessor_config);
-
-    let img = image::ImageReader::open(image_file_path)?.decode()?;
-    let image_tensor = image_processor.preprocess(&img)?.unsqueeze(0)?;
-    let image_tensor = image_tensor.to_dtype(DType::BF16)?.to_device(&device)?;
-    // println!("Image size: {:?}", (img.width(), img.height()));
-    // println!("Image tensor: {:?}", image_tensor);
-    Ok(((img.width(), img.height()), image_tensor))
-}
-
-fn load_clip() -> anyhow::Result<(ClipVisionConfig,)> {
-    let clip_vision_config = ClipVisionConfig::clip_vit_large_patch14_336();
-    Ok((clip_vision_config,))
-}
-
-fn duplicate_vec<T>(vec: &[T], n: usize) -> Vec<T>
-where
-    T: Clone,
-{
-    let mut res = Vec::new();
-    for _ in 0..n {
-        res.extend(vec.to_owned());
-    }
-    res
-}
-
-fn insert_separator<T>(x: Vec<Vec<T>>, sep: Vec<T>) -> Vec<Vec<T>>
-where
-    T: Clone,
-{
-    let sep = vec![sep];
-    let sep = duplicate_vec(&sep, x.len());
-    let mut res = x
-        .iter()
-        .zip(sep.iter())
-        .flat_map(|(x, y)| vec![x.clone(), y.clone()])
-        .collect::<Vec<Vec<T>>>();
-    res.pop();
-    res
-}
-
-fn tokenizer_image_token(
-    prompt: &str,
-    tokenizer: &Tokenizer,
-    image_token_index: i64,
-    bos_token_id: i64,
-) -> anyhow::Result<Tensor> {
-    let prompt_chunks = prompt
-        .split("<image>")
-        .map(|s| {
-            tokenizer
-                .encode(s, true)
-                .unwrap()
-                .get_ids()
-                .to_vec()
-                .iter()
-                .map(|x| *x as i64)
-                .collect()
-        })
-        .collect::<Vec<Vec<i64>>>();
-    let mut input_ids = Vec::new();
-    let mut offset = 0;
-    if !prompt_chunks.is_empty()
-        && !prompt_chunks[0].is_empty()
-        && prompt_chunks[0][0] == bos_token_id
-    {
-        offset = 1;
-        input_ids.push(prompt_chunks[0][0]);
-    }
-
-    for x in insert_separator(
-        prompt_chunks,
-        duplicate_vec(&[image_token_index], offset + 1),
-    )
-    .iter()
-    {
-        input_ids.extend(x[1..].to_vec())
-    }
-    // println!("input_ids: {:?}", input_ids);
-    let input_len = input_ids.len();
-    Tensor::from_vec(input_ids, (1, input_len), &Device::Cpu).map_err(anyhow::Error::msg)
-}
-
-/// image 564 564
-/// x shape: Tensor[dims 1, 3, 336, 336; bf16, metal:4294969862]
-/// clip_vision_tower result shape: Tensor[dims 1, 576, 1024; bf16, metal:4294969862]
-/// mm_projector result shape: Tensor[dims 1, 576, 3072; bf16, metal:4294969862]
-fn encode_images(x: &Tensor) -> anyhow::Result<Tensor> {
-    println!("x shape: {:?}", x);
-    // let image_features = self.clip_vision_tower.forward(x)?;
-    // let image_features = self.mm_projector.forward(&image_features)?;
-    // Ok(image_features)
-    Ok(x.to_owned())
-}
-
-fn prepare_inputs_labels_for_multimodal(
-    device: &Device,
-    qllama: &QLlama,
-    input_ids: &Tensor,
-    images: &[Tensor],
-    _image_sizes: &[(u32, u32)],
-    image_token_index: i64,
-) -> anyhow::Result<Tensor> {
-    let concat_images = Tensor::cat(images, 0)?;
-    let image_features_together = encode_images(&concat_images)?;
-    let split_sizes = images
-        .iter()
-        .map(|x| x.shape().dims()[0])
-        .collect::<Vec<usize>>();
-    // can be replaced by split
-    let mut index_pos = 0;
-    let mut image_features = Vec::new();
-    for split_size in split_sizes.iter() {
-        image_features.push(image_features_together.i(index_pos..index_pos + (*split_size))?);
-        index_pos += *split_size;
-    }
-
-    let image_features = image_features
-        .iter()
-        .map(|x| x.flatten(0, 1).unwrap())
-        .collect::<Vec<Tensor>>();
-
-    let input_ids_vec = input_ids.squeeze(0)?.to_vec1::<i64>()?;
-    let mut image_indices = {
-        let mut image_indices = vec![0_i64];
-        image_indices.extend(
-            input_ids_vec
-                .iter()
-                .enumerate()
-                .filter_map(|(i, x)| {
-                    if *x == image_token_index {
-                        Some(i as i64)
-                    } else {
-                        None
-                    }
-                })
-                .collect::<Vec<i64>>(),
-        );
-        image_indices
-    };
-
-    // if image_indices.len() == 1 {
-    //     //no image, only [0],
-    //     return self.llama.embed(input_ids);
-    // }
-
-    let input_ids_noim = input_ids_vec
-        .iter()
-        .filter_map(|x| {
-            if *x != image_token_index {
-                Some(*x)
-            } else {
-                None
-            }
-        })
-        .collect::<Vec<i64>>();
-    let input_ids_noim_len = input_ids_noim.len();
-    image_indices.push((input_ids_noim_len) as i64);
-    let input_ids_noim = Tensor::from_vec(input_ids_noim, input_ids_noim_len, &device)?;
-    // println!("input_ids_noim: {:?}", input_ids_noim);
-    let cur_input_embeds = qllama.model.embed(&input_ids_noim)?;
-    // println!("cur_input_embeds: {:?}", cur_input_embeds);
-    // can be replace by split if it is implemented in candle
-    let input_embed_no_ims = {
-        let mut input_embeds = Vec::new();
-        for i in 0..image_indices.len() - 1 {
-            let start = (image_indices[i]) as usize;
-            let end = image_indices[i + 1] as usize;
-            input_embeds.push(cur_input_embeds.i((start..end, ..))?)
-        }
-        input_embeds
-    };
-    let mut cur_new_input_embeds = Vec::new();
-    for (i, _image_feature) in image_features.iter().enumerate() {
-        cur_new_input_embeds.push(input_embed_no_ims[i].clone());
-        // TODO encode_images 还没实现 projection, 这里先不放进去, 不然 shape 不对
-        // cur_new_input_embeds.push(image_feature.clone());
-    }
-    cur_new_input_embeds.push(input_embed_no_ims[image_features.len()].clone());
-    let new_input_embeds = Tensor::cat(&cur_new_input_embeds, 0)?;
-
-    // trancate
-    let tokenizer_model_max_length = Some(4096); // in models/llava-phi-3/tokenizer_config.json
-    let new_input_embeds = if let Some(tokenizer_model_max_length) = tokenizer_model_max_length {
-        let (new_input_embeds_length, _) = new_input_embeds.shape().dims2()?;
-        if new_input_embeds_length > tokenizer_model_max_length {
-            new_input_embeds.i((..tokenizer_model_max_length, ..))?
-        } else {
-            new_input_embeds
-        }
-    } else {
-        new_input_embeds
-    };
-
-    Ok(new_input_embeds.unsqueeze(0)?)
-}
-
 pub async fn run() -> anyhow::Result<()> {
     let device = Device::new_metal(0)?;
     let qllama = load_qllama_model(
@@ -348,12 +141,12 @@ pub async fn run() -> anyhow::Result<()> {
 
     let (_clip_vision_config,) = load_clip()?;
 
-    let image_token_index: i64 = 32038; // see tokenizer.json
+    let image_token_id: i64 = 32038; // see tokenizer.json
     let bos_token_id: i64 = 1;
     let tokens = tokenizer_image_token(
         prompt_str.as_str(),
         &qllama.tokenizer,
-        image_token_index,
+        image_token_id,
         bos_token_id,
     )?;
 
@@ -363,7 +156,7 @@ pub async fn run() -> anyhow::Result<()> {
         &tokens,
         &[image_tensor],
         &[image_size],
-        image_token_index,
+        image_token_id,
     )?;
 
     generate(&device, input_embeds, qllama)?;
--- src/qllava/mod.rs
+++ src/qllava/mod.rs
@@ -1,5 +1,10 @@
 mod qllama;
-pub(super) use qllama::*;
+pub(super) use qllama::{format_prompt, load_qllama_model, QLlama};
 
 mod image_processor;
-pub(super) use image_processor::{HFPreProcessorConfig, ImageProcessor};
+// pub(super) use image_processor::{HFPreProcessorConfig, ImageProcessor};
+
+mod clip;
+pub(super) use clip::{
+    load_clip, load_image_to_tensor, prepare_inputs_labels_for_multimodal, tokenizer_image_token,
+};
