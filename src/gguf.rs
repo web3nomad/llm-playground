57b195de38c2e8fb64371a80c4d04e762ca792b3
web3nomad.eth <me@web3nomad.eth>
2024-10-17 16:30:14 +0800

    remove generate legacy

--- src/gguf.rs
+++ src/gguf.rs
@@ -7,78 +7,6 @@ use candle_examples::token_output_stream::TokenOutputStream;
 use candle_transformers::{generation::LogitsProcessor, models::quantized_llama};
 use std::io::Write;
 
-pub fn generate_legacy(
-    device: &Device,
-    prompt_str: &str,
-    mut qllama: QLlama,
-) -> anyhow::Result<()> {
-    let mut tos = TokenOutputStream::new(qllama.tokenizer);
-    let to_sample = qllama.sample_len.saturating_sub(1);
-
-    let prompt_tokens = {
-        let tokens = tos
-            .tokenizer()
-            .encode(prompt_str, true)
-            .map_err(anyhow::Error::msg)?;
-        let prompt_tokens = tokens.get_ids().to_vec();
-        if prompt_tokens.len() + to_sample > quantized_llama::MAX_SEQ_LEN - 10 {
-            let to_remove = prompt_tokens.len() + to_sample + 10 - quantized_llama::MAX_SEQ_LEN;
-            prompt_tokens[prompt_tokens.len().saturating_sub(to_remove)..].to_vec()
-        } else {
-            prompt_tokens
-        }
-    };
-
-    let mut logits_processor = LogitsProcessor::from_sampling(qllama.seed, qllama.sampling);
-
-    let mut next_token = {
-        let input = Tensor::new(prompt_tokens.as_slice(), &device)?.unsqueeze(0)?;
-        let logits = qllama.model.forward(&input, 0)?;
-        let logits = logits.squeeze(0)?;
-        logits_processor.sample(&logits)?
-    };
-
-    let mut all_tokens = vec![];
-    all_tokens.push(next_token);
-    if let Some(t) = tos.next_token(next_token)? {
-        print!("{t}");
-        std::io::stdout().flush()?;
-    }
-
-    for index in 0..to_sample {
-        let input = Tensor::new(&[next_token], &device)?.unsqueeze(0)?;
-        let logits = qllama.model.forward(&input, prompt_tokens.len() + index)?;
-        let logits = logits.squeeze(0)?;
-        // let logits = if qllama.repeat_penalty == 1. {
-        //     logits
-        // } else {
-        //     let start_at = all_tokens.len().saturating_sub(qllama.repeat_last_n);
-        //     candle_transformers::utils::apply_repeat_penalty(
-        //         &logits,
-        //         qllama.repeat_penalty,
-        //         &all_tokens[start_at..],
-        //     )?
-        // };
-        next_token = logits_processor.sample(&logits)?;
-        all_tokens.push(next_token);
-        if let Some(t) = tos.next_token(next_token)? {
-            print!("{t}");
-            std::io::stdout().flush()?;
-        }
-        if next_token == qllama.eos_token_id {
-            break;
-        };
-    }
-
-    if let Some(rest) = tos.decode_rest().map_err(candle_core::Error::msg)? {
-        print!("{rest}");
-    }
-    std::io::stdout().flush()?;
-    println!("");
-
-    Ok(())
-}
-
 pub fn generate(
     device: &Device,
     mut input_embeds: Tensor,
@@ -166,7 +94,6 @@ pub async fn run() -> anyhow::Result<()> {
     )?;
 
     generate(&device, input_embeds, qllama)?;
-    // generate_legacy(&device, prompt_str.as_str(), qllama)?;
 
     Ok(())
 }
